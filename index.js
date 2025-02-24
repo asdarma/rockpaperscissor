d7f27a7c365d7ad2a4b9574fcd427fa89ff8395d
Darma <meet.darma@gmail.com>
2025-02-24 21:44:54 +0700

    Step 1, 2, 3 & 4 done

---
+++ index.js
@@ -0,0 +1,27 @@
+// 1. Setup the project structure
+console.log("Welcome to Rock Paper Scissor game");
+
+// 2. Write the logic to get the computer choice
+function getComputerChoice() {
+    const compRandom = Math.floor(Math.random() * 3);
+    if ( compRandom === 1) {
+        return 'rock';
+    } else if ( compRandom === 2) {
+        return 'paper';
+    } else {
+        return 'scissor';
+    }
+}
+console.log('Computer choice is: ' + getComputerChoice());
+
+// 3. Write the logic to get the human choice
+function getHumanChoice() {
+    const humanChoice = prompt('Enter your choice: rock, paper or scissor').toLowerCase();
+    return humanChoice;
+}
+console.log('Your choice is: '+ getHumanChoice());
+
+// 4. Declare the players score variables
+
+let humanScore = 0;
+let compScore = 0;

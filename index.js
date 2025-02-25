7fe19f0f509566fd3a9e09ebe3914822d9f81cc3
Darma <meet.darma@gmail.com>
2025-02-25 16:38:07 +0700

    Done adding three buttons, div for displaying the results and runs the logic of the game.

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+// branch rps-ui
+// 1.b. remove logic that plays exactly five rounds.
+// function playGame() {
+//     const rounds = 6;
+//     for ( let round = 1; round < rounds; round++ ) {
+//     console.log(`Round: ${round}`)
+//     }
+// }
+
+// 2.b. Create three buttons; rock, paper, scissor.
+// Add event listener that call playRound()
+// Done in index.html file
+
+// 3.b. Add a <div> for displaying result and change all console.log into DOM
+// console.log('Result: '+ playRound(humanSelection, compSelection));
+// console.log('Your choice is: ' + humanSelection);
+// console.log('Computer choice is: ' + compSelection);
+// console.log('Your score: ' + humanScore);
+// console.log('Computer score: ' + compScore);
 // 1. Setup the project structure
-console.log("Welcome to Rock Paper Scissor game");
+// console.log("Welcome to Rock Paper Scissor game");
+// 3. Write the logic to get the human choice
+
+// 4.b. Display the running score and annouce a winner once reach 5 point
+// 5.b. Rework/rewrite original code
+
+const buttons = document.querySelectorAll(".buttons button");
+const resultText = document.getElementById("resultText");
+const resultHuman = document.getElementById("resultHuman");
+const resultComp = document.getElementById("resultComp");
 
 // 2. Write the logic to get the computer choice
 function getComputerChoice() {
@@ -13,25 +41,10 @@ function getComputerChoice() {
     }
 }
 
-// 3. Write the logic to get the human choice
-function getHumanChoice() {
-    const humanInput = prompt('Enter your choice: rock, paper or scissor').toLowerCase();
-    return humanInput;
-}
-
 // 4. Declare the players score variables
 let humanScore = 0;
 let compScore = 0;
-
-// 5. Write the logic to play a single round
-// 6. Write the logic to play the entire game
-function playGame() {
-    const rounds = 6;
-    for ( let round = 1; round < rounds; round++ ) {
-    console.log(`Round: ${round}`)
-    
-
-
+  
     function playRound(humanChoice, compChoice) {
         if ( humanChoice === compChoice ) {
             return 'Its a Tie round';
@@ -48,21 +61,22 @@ function playGame() {
         };
     };
 
-    const humanSelection = getHumanChoice();
-    const compSelection = getComputerChoice();
+buttons.forEach( button => {
+    button.addEventListener('click', () => {
 
-    console.log('Result: '+ playRound(humanSelection, compSelection));
-    console.log('Your choice is: ' + humanSelection);
-    console.log('Computer choice is: ' + compSelection);
-    console.log('Your score: ' + humanScore);
-    console.log('Computer score: ' + compScore);
+        const humanChoice = button.id;
+        const compChoice = getComputerChoice();
+        const result = playRound(humanChoice, compChoice);
 
-    };
-};
-playGame();
+        resultHuman.textContent = `YOUR choice: ${humanChoice}. [score: ${humanScore}]`;
+        resultComp.textContent = `COMPUTER choice: ${compChoice}. [score: ${compScore}]`;
+        resultText.textContent = `GAME RESULT: ${result}`;
 
-if ( compScore < humanScore ) {
-    console.log('Congratulations! You are the CHAMPION')
-} else {
-    console.log('Unfortunately, Computer is the Champion.')
-};
+    });
+});
+
+// if ( compScore < humanScore ) {
+//     console.log('Congratulations! You are the CHAMPION')
+// } else {
+//     console.log('Unfortunately, Computer is the Champion.')
+// };
